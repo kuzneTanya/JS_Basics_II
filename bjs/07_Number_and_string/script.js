0d2daba207f744646aee797784bd57d0a300352a
Tatyana Kuznetsova <kuznetsova.t.s.nmc@gmail.com>
2024-10-11 15:44:37 +0500

    start work with calc

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,12 +1,21 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+const inputWindow = document.querySelector('#inputWindow');
 
-
-document.getElementById('btn_clr').addEventListener('click', function () {
+document.querySelector('#btn_sum').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
+})
+
+document.querySelector('#btn_clr').addEventListener('click', function () {
+    lastOperand = 0;
+    operation = null;
+    inputWindow.value = '';
+})
+
+document.querySelector('#btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
 })
 
